8e0360e7838b2c94603400deb458cc0712ce0f00
mavseven <138080403+mavseven@users.noreply.github.com>
2023-08-05 07:28:50 +0700

    Update jquery.back-to-top.js

--- b/jquery.back-to-top.js
+++ b/jquery.back-to-top.js
@@ -1,14 +1,14 @@
 $(document).ready(function($){
-    // browser window scroll (in pixels) after which the "back to top" link is shown
+    // gulir jendela browser (dalam piksel) setelah tautan "kembali ke atas" ditampilkan
     var offset = 300,
-        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        //gulir jendela browser (dalam piksel) setelah itu opasitas tautan "kembali ke atas" dikurangi
         offset_opacity = 1200,
-        //duration of the top scrolling animation (in ms)
+        //durasi animasi pengguliran atas (dalam md)
         scroll_top_duration = 700,
-        //grab the "back to top" link
+        //ambil tautan "kembali ke atas".
         $back_to_top = $('.js-back-to-top');
 
-    //hide or show the "back to top" link
+    //sembunyikan atau tampilkan tautan "kembali ke atas".
     $(window).scroll(function(){
         ( $(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible back-to-top-fade-out');
         if( $(this).scrollTop() > offset_opacity ) {
@@ -16,7 +16,7 @@ $(document).ready(function($){
         }
     });
 
-    //smooth scroll to top
+    //gulir halus ke atas
     $back_to_top.on('click', function(event){
         event.preventDefault();
         $('body,html').animate({
